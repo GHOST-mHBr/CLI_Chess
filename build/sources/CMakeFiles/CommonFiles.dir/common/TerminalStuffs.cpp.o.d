4b846cf5989f6c10dc8d57f25b8d98794017689c
GHOST-mHBr <ghostmhbr@gmail.com>
2024-11-06 20:15:58 +0330

    gitignore

--- build/sources/CMakeFiles/CommonFiles.dir/common/TerminalStuffs.cpp.o.d
+++
@@ -1,3 +0,0 @@
-sources/CMakeFiles/CommonFiles.dir/common/TerminalStuffs.cpp.o: \
- /home/arch/Desktop/Codes/C++/Rasta/final/sources/common/TerminalStuffs.cpp \
- /usr/include/stdc-predef.h
--- build/sources/CMakeFiles/CommonFiles.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for CommonFiles.
-# This may be replaced when dependencies are built.
